432aecfd3d43c71a7985807bdc5f2b74625c89f1
muslimovzeynal <43483359+muslimovzeynal@users.noreply.github.com>
2018-09-22 20:16:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     
     changeState(document.getElementsByClassName('main__block'));
     
-    var spanList = document.getElementsByClassName('main__block__p8__span1');
+    var spanList = document.getElementsByClassName('main__p8__span1');
     for (i=0; i < spanList.length; i++){
         spanList[i].addEventListener('click', function(e) {
             if (!this.parentElement.parentElement.getElementsByClassName('main__block')[0].classList.contains('disabled')) { this.parentElement.parentElement.getElementsByClassName('main__block')[0].classList.toggle('selected');
@@ -28,19 +28,19 @@ function changeState(blockList) {
     
     for (i=0; i < blockList.length; i++){
         if (blockList[i].classList.contains('selected')) {
-            blockList[i].parentElement.getElementsByClassName('main__block__p8_selected')[0].style.display = 'block';
-            blockList[i].parentElement.getElementsByClassName('main__block__p8')[0].style.display = 'none';
-            blockList[i].parentElement.getElementsByClassName('main__block__p8_disabled')[0].style.display = 'none';
+            blockList[i].parentElement.getElementsByClassName('main__p8_selected')[0].style.display = 'block';
+            blockList[i].parentElement.getElementsByClassName('main__p8')[0].style.display = 'none';
+            blockList[i].parentElement.getElementsByClassName('main__p8_disabled')[0].style.display = 'none';
         }
         else if (blockList[i].classList.contains('disabled')) {
-            blockList[i].parentElement.getElementsByClassName('main__block__p8_disabled')[0].style.display = 'block';
-            blockList[i].parentElement.getElementsByClassName('main__block__p8')[0].style.display = 'none';
-            blockList[i].parentElement.getElementsByClassName('main__block__p8_selected')[0].style.display = 'none';
+            blockList[i].parentElement.getElementsByClassName('main__p8_disabled')[0].style.display = 'block';
+            blockList[i].parentElement.getElementsByClassName('main__p8')[0].style.display = 'none';
+            blockList[i].parentElement.getElementsByClassName('main__p8_selected')[0].style.display = 'none';
         }
         else {
-            blockList[i].parentElement.getElementsByClassName('main__block__p8')[0].style.display = 'block';
-            blockList[i].parentElement.getElementsByClassName('main__block__p8_selected')[0].style.display = 'none';
-            blockList[i].parentElement.getElementsByClassName('main__block__p8_disabled')[0].style.display = 'none';
+            blockList[i].parentElement.getElementsByClassName('main__p8')[0].style.display = 'block';
+            blockList[i].parentElement.getElementsByClassName('main__p8_selected')[0].style.display = 'none';
+            blockList[i].parentElement.getElementsByClassName('main__p8_disabled')[0].style.display = 'none';
         }
 
     };
